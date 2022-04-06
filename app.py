1eb43c8376f83aeac58fa65a5c695d3c2ed56e62
Sharon Liu <s120@umbc.edu>
2022-04-05 20:57:39 -0400

    uses new urls, slow but working state and county filters

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import requests             # used for downloading files from websites
 import pandas as pd         # used for csv
-from flask import *
+from flask import *         # used for displaying website
 
 # global constants
 URL_INFECT = 'https://opendata.maryland.gov/api/views/tm86-dujs/rows.csv?accessType=DOWNLOAD'
@@ -17,11 +17,12 @@ URL_DATE_STATES = 'https://api.covidactnow.org/v2/states.timeseries.csv?apiKey=f
 URL_DATE_COUNTIES = 'https://api.covidactnow.org/v2/counties.timeseries.csv?apiKey=fc20e15ab3024b65939c396e1c2f0761'
 
 # global variables
+data_master = None
 data_infect = None
 data_vaccine = None
 
-header_infect = None
-header_vaccine = None
+list_counties = None
+list_states = None
 
 headings = ('Category', 'Value')
 data = []
@@ -38,23 +39,20 @@ def my_request():
     :return:
     """
     # make sure to use global variables instead of initializing new local ones
-    global data_infect, data_vaccine, header_infect, header_vaccine
+    global data_master, data_infect, data_vaccine, list_states, list_counties
 
-    # open and convert csv into a dataframe
-    # data_infect = pd.read_csv(CSV_INFECT)
-    # data_vaccine = pd.read_csv(CSV_VACCINE)
+    # only get the needed columns or else it would take too long to load the entire csv
+    data_master = pd.read_csv(URL_DATE_COUNTIES, usecols=['date', 'state', 'county', 'actuals.cases', 'actuals.vaccinationsCompleted'])
+    #data_infect = data_master[['date', 'state', 'county', 'actuals.cases']]
+    #data_vaccine = data_master[['date', 'state', 'county', 'actuals.vaccinationsCompleted']]
 
-    data_infect = pd.read_csv(URL_INFECT)
-    data_vaccine = pd.read_csv(URL_VACCINE)
+    # get list of states
+    list_states = pd.read_csv(URL_CURR_STATES, usecols=['state'])
+    list_states = list_states['state'].to_numpy()
 
-    # drop the OBJECTID and Unknown columns in CSV_INFECT 
-    data_infect = data_infect.drop('OBJECTID', axis=1)
-    data_infect = data_infect.drop('Unknown', axis=1)
+    # get list of counties with their states
+    list_counties = pd.read_csv(URL_CURR_COUNTIES, usecols=['state', 'county'])
 
-    # store header names
-    header_infect = data_infect.columns
-    header_vaccine = data_vaccine.columns
-    
 
 @app.route('/', methods=('GET', 'POST'))
 def home_page():
@@ -63,41 +61,44 @@ def home_page():
     :return:
     """
     if request.method == 'GET':
-        if (data_infect is None) and (data_vaccine is None):
+        if data_master is None:
             # load dataframe from csv
             my_request()
 
-        # run home page
-        return render_template('home.html', headers=header_infect)
+        # run home page, set headers to dropdown list options
+        return render_template('home.html', states=list_states)
     
     # post method
     else:
-        countyfrominput = request.form['county']
-        return redirect(url_for("select_county", county=countyfrominput))
+        input_state = request.form['state']
+        return redirect(url_for("select_county", state=input_state))
 
 
-@app.route('/get/<county>', methods=('GET', 'POST'))
-def select_county(county):
+@app.route('/get/<state>', methods=('GET', 'POST'))
+def select_county(state):
     """
     Link to select county
     :return:
     """
+    if request.method == 'GET':
+        # get a list of counties specific to chosen state
+        state_counties = list_counties.loc[list_counties['state'] == state]
+        state_counties = state_counties['county'].to_numpy()
 
+        # run search county page, set headers to dropdown list options
+        return render_template('select_county.html', state=state, counties=state_counties)
 
-    # chosen county
-    # county = "Baltimore"
+    # post method
+    else:
+        # receive input on which county to select
+        input_county = request.form['county']
 
-    # search for chosen county
-    if(search_county_infected(county) is None):
-        return redirect(url_for('county_error', county=county))
+        # search for chosen county
+        if (search_county(state, input_county) is None):
+            return redirect(url_for('county_error', county=input_county))
 
-
-
-    # run select page
-    #return render_template('select_county.html')
-
-    # for now send directly to county dataframe page
-    return redirect(url_for('county', county=county))
+        # go to county dataframe page
+        return redirect(url_for('county', county=input_county))
 
 
 @app.route('/get/county/<county>')
@@ -117,7 +118,7 @@ def county_error(county):
     return redirect(url_for('home_page'))
 
 
-def calc_county_infected(county, dataframe):
+def county_stats(dataframe):
     """
     Displays dataframe for chosen county
     (currently as a table in alphabetical order)
@@ -132,18 +133,21 @@ def calc_county_infected(county, dataframe):
     global data
 
     # calculate statistics for chosen county
-    cumulative = dataframe[county].sum()
-    last_7 = dataframe[county].tail(7).sum()
+    cases_cumulative = dataframe['actuals.cases'].values[-1]
+    cases_last_7 = dataframe['actuals.cases'].values[-1] - dataframe['actuals.cases'].values[-7]
+    vac_cumulative = dataframe['actuals.vaccinationsCompleted'].values[-1]
+    vac_last_7 = dataframe['actuals.vaccinationsCompleted'].values[-1] - dataframe['actuals.vaccinationsCompleted'].values[-7]
 
     # create table to be displayed
     data = [
-        ('Cumulative cases', str(cumulative)),
-        ('Last 7 days', str(last_7))
+        ('Cumulative cases', str(cases_cumulative)),
+        ('Cases last 7 days', str(cases_last_7)),
+        ('Cumulative vaccinations', str(vac_cumulative)),
+        ('Vaccinations last 7 days', str(vac_last_7))
     ]
 
 
-
-def search_county_infected(county):
+def search_county(state, county):
     """
     Finds the given county in the infected dataframe
     Returns error if not found
@@ -151,16 +155,33 @@ def search_county_infected(county):
     :return:
     """
 
+    # get a list of counties specific to chosen state
+    state_counties = list_counties.loc[list_counties['state'] == state]
+
+    # get a list of entries for that specific county
+    state_counties = state_counties.loc[state_counties['county'] == county]
+
+    # convert dataframe into a list
+    state_counties = state_counties['county'].to_numpy()
+
     # attempt to find chosen county
-    if county in header_infect:
+    if county in state_counties:
         # if county is found, create new dataframe for just that county
-        data_county = data_infect[['DATE', county]]
+
+        # get a list of entries specific to chosen state
+        data_county = data_master.loc[data_master['state'] == state]
+
+        # get a list of entries for that specific county
+        data_county = data_county.loc[data_county['county'] == county]
                 
         # remove NA entries of that county
         data_county = data_county.dropna()
 
+        # sort entries by date
+        data_county = data_county.sort_values(by='date')
+
         # print dataframe of chosen county
-        calc_county_infected(county, data_county)
+        county_stats(data_county)
 
         return 1
     else:
